d4c1aac6f4ce9de2a01c12256a2fe42e9ada808d
Khavushka <khavushka@gmail.com>
2021-02-23 11:31:09 +0100

    express

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
 const text = 'Hello NodeJS'
 
 module.exports = text
+
+console.log("Hello");
--- js0.js
+++ js0.js
@@ -1,5 +1,39 @@
-const chalk = require('chalk')
-const text = require('./data.js')
+// const http = require('http');
+// const url = require('url');
+// const { parse } = require('querystring');
 
-console.log(chalk.blue('Hello Node'))
-console.log(__dirname)
+// http.createServer( (request, response) => {
+//     console.log('server work');
+//      if(request.method == 'GET') {
+//          //GET method
+//     console.log(request.method); // request
+//     let urlRequest = url.parse(request.url, true);
+//     // console.log(urlRequest);
+//     console.log(urlRequest.query.test);
+//     if (urlRequest.query.test % 2 == 0) {
+//         response.end('even');
+//     }
+//     response.end('odd');
+// }
+// else {
+//     // POST method
+//     let body = '';
+//     request.on('data', chunk => {
+//         body += chunk.toString();
+//     });
+//     request.on('end', () => {
+//         console.log(body);
+//         let params = parse(body);
+//         console.log(params);
+//         console.log(params.hi);
+//         response.end('ok');
+//     });
+// }
+// }).listen(3000);
+
+const fs = require('fs');
+
+//1 vej
+fs.readFile('t1.txt', 'uft-8', (err, data) => {
+    console.log(data);
+});
